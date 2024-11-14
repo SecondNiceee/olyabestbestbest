1b5ab84e22552fa5505dc11e3aaf53d80a068b28
SecondNiceee <col1596321@gmail.com>
2024-11-14 03:07:29 +0300

    dasdas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState("")
+  const [isShow, setShow] = useState({message : ""})
+  const handler = () => {
+    if (state.toLocaleLowerCase() === "коля"){
+      setShow({message : "Нет, но она его Хозяйка!!"})
+    }
+    else{
+
+      if (state.toLocaleLowerCase() === "оля"){
+        setShow({message : "Да!!, она самая лучшая!"})
+      }
+      else{
+        setShow(null)
+      }
+    }
+    
+  }
+  console.log(isShow)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='container'>
+          <h2 className='title'>Кто лучший??</h2>
+          <input value={state} onChange={(e) => {setState(e.target.value)}} className='input' />
+          <button className='button' onClick={handler}>Проверить</button>
+          {state.toLocaleLowerCase() === "коля" && <p className='is-show'>{"Нет, но она его Хозяйка!!"}</p>}
+          {state.toLocaleLowerCase() === "оля" && <p className='is-show'>{"Да!!, она самая лучшая"}</p>}
+        </div>
       </header>
     </div>
   );
