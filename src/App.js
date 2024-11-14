5fef8c6bd5d8650593f49828726647bfe5f4b249
SecondNiceee <col1596321@gmail.com>
2024-11-14 03:49:48 +0300

    dsa

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
   const [state, setState] = useState("")
   const [isShow, setShow] = useState({message : ""})
   const handler = () => {
-    if (state.toLocaleLowerCase() === "коля"){
+    if (state.toLocaleLowerCase().includes("коля")){
       setShow({message : "Нет, но она его Хозяйка!!"})
     }
     else{
 
-      if (state.toLocaleLowerCase() === "оля"){
+      if (state.toLocaleLowerCase().includes("оля")){
         setShow({message : "Да!!, она самая лучшая!"})
       }
       else{
@@ -20,18 +20,17 @@ function App() {
     }
     
   }
-  console.log(isShow)
   return (
     <div className="App">
-      <header className="App-header">
+
         <div className='container'>
           <h2 className='title'>Кто лучший??</h2>
           <input value={state} onChange={(e) => {setState(e.target.value)}} className='input' />
           <button className='button' onClick={handler}>Проверить</button>
-          {state.toLocaleLowerCase() === "коля" && <p className='is-show'>{"Нет, но она его Хозяйка!!"}</p>}
-          {state.toLocaleLowerCase() === "оля" && <p className='is-show'>{"Да!!, она самая лучшая"}</p>}
+          <p className= {`${state.toLocaleLowerCase() === "коля" ? 'active' : ''} is-show`}>{"Нет, но она его Хозяйка!! А он просто песик"}</p>
+          <p className= {`${state.toLocaleLowerCase() === "оля" ? 'active' : ''} is-show`}>{"Да!! Это она Чудо!"}</p>
+
         </div>
-      </header>
     </div>
   );
 }
